824675dd4483b6cb09fa95c63311b5708b8bb43e
ryo_kurosu <dtrjs.kurosu.0510@gmail.com>
2022-04-27 11:33:38 +0900

    多項式の基底関数を実装
    ＋関数化

--- 0513_regression_analysis.py
+++ 0513_regression_analysis.py
@@ -11,6 +11,20 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
 
 
+def polynomial_regression(X_train, Y_train, X_test, Y_test):
+    # 実行(polynomial function)
+    for deg in range(1, 10):
+        model_poly = Pipeline([
+            ('poly', PolynomialFeatures(degree=deg)),
+            ('linear', LinearRegression())
+        ])
+        model_poly.fit(X_train, Y_train)
+        print(f"=====model poly {deg}=====")
+        print(f'train_score: {model_poly.score(X_train, Y_train)}')
+        print(f'test_score: {model_poly.score(X_test, Y_test)}')
+
+    return 0
+
 
 def main():
     df = pd.read_csv("winequality-red.csv", sep=";")
@@ -19,47 +33,12 @@ def main():
 
     X = df[["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides", "free sulfur dioxide",
             "total sulfur dioxide", "density", "pH", "sulphates", "sulphates", "alcohol"]]
+    # X = df[["volatile acidity", "citric acid", "chlorides", "alcohol"]]
+
     Y = df[["quality"]]
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
 
-    # 標準化(分散)
-    # -numpy-
-    # X_train_np = X_train.apply(lambda x: (x - np.mean(x)) / np.std(x))
-    # X_test_np = X_test.apply(lambda x: (x - np.mean(x)) / np.std(x))
-    # Y_train_np = Y_train.apply(lambda x: (x - np.mean(x)) / np.std(x))
-    # Y_test_np = Y_test.apply(lambda x: (x - np.mean(x)) / np.std(x))
-
-    # print(x_np.head())
-    # print(y_np.head())
-    # print(x_np.std(ddof=0))
-
-    # -sklearn-
-    # sscaler = preprocessing.StandardScaler()
-    # sscaler.fit(X)
-    # xss_sk = sscaler.transform(X)
-    # sscaler.fit(Y)
-    # yss_sk = sscaler.transform(Y)
-
-
-    # 実行
-    # -標準化なし-
-    model = LinearRegression()
-    model.fit(X_train, Y_train)
-
-    # 結果
-    print("b: ", model.intercept_)
-    print("coef: ", model.coef_)
-    print(f'train_score: {model.score(X_train, Y_train)}')
-    print(f'test_score: {model.score(X_test, Y_test)}')
-
-    # -標準化あり-
-    # model_std = LinearRegression()
-    # model_std.fit(X_train_np, Y_train_np)
-    #
-    # print(model_std.coef_)
-    # print(model_std.intercept_)
-    # print(f'train_score: {model_std.score(X_train_np, Y_train_np)}')
-    # print(f'test_score: {model_std.score(X_test_np, Y_test_np)}')
+    polynomial_regression(X_train, Y_train, X_test, Y_test)
 
 
 if __name__ == "__main__":
