435c6820ce7639d7894e254d47571e2ab664f6e1
ryo_kurosu <dtrjs.kurosu.0510@gmail.com>
2022-04-30 21:23:23 +0900

    相関ヒートマップ、プロットを追加
    Lassoだけプロットできない

--- 0513_regression_analysis.py
+++ 0513_regression_analysis.py
@@ -5,12 +5,32 @@ import seaborn as sns
 
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression, Lasso
-from mpl_toolkits.mplot3d import Axes3D
-from sklearn import preprocessing
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import r2_score
 
 sns.set()  # seabornのスタイルをセット
+
+
+def linear_regression(X_train, Y_train, X_test, Y_test):
+    # 線形回帰
+    model_linear = LinearRegression()
+    model_linear.fit(X_train, Y_train)
+
+    print(f"---linear regression---")
+    print(f'train_score: {model_linear.score(X_train, Y_train)}')
+    print(f'test_score: {model_linear.score(X_test, Y_test)}')
+
+    Y_train_pred = model_linear.predict(X_train)
+    Y_test_pred = model_linear.predict(X_test)
+
+    plot_func(Y_train, Y_train_pred, Y_test, Y_test_pred)
+
+    print(r2_score(Y_train, Y_train_pred))
+    print(r2_score(Y_test, Y_test_pred))
+
+
+    return
 
 
 def polynomial_regression(X_train, Y_train, X_test, Y_test, X, Y):
@@ -25,9 +45,13 @@ def polynomial_regression(X_train, Y_train, X_test, Y_test, X, Y):
         print(f'train_score: {model_poly.score(X_train, Y_train)}')
         print(f'test_score: {model_poly.score(X_test, Y_test)}')
 
-        # calculate_coer(X.T, Y)
+        Y_train_pred = model_poly.predict(X_train)
+        Y_test_pred = model_poly.predict(X_test)
 
-    return 0
+        plot_func(Y_train, Y_train_pred, Y_test, Y_test_pred)
+
+
+    return
 
 
 # 多項式基底関数を作成
@@ -62,17 +86,60 @@ def lasso_regression(X_train, Y_train, X_test, Y_test):
         print(f'train score : {lasso_model.score(X_train, Y_train)}')
         print(f'test score : {lasso_model.score(X_test, Y_test)}')
 
+        Y_train_pred = lasso_model.predict(X_train)
+        Y_test_pred = lasso_model.predict(X_test)
+
+        # plot_func(Y_train, Y_train_pred, Y_test, Y_test_pred)
+
     return
 
 
 def calculate_coer(X, Y):
+    df = pd.read_csv("winequality-red.csv", sep=";")
     # 相関係数を計算
-    corr = np.corrcoef(X.T, Y)
+    corr = df[["fixed acidity", "volatile acidity", "citric acid", "residual sugar", "chlorides", "free sulfur dioxide", "total sulfur dioxide", "density", "pH", "sulphates", "sulphates", "alcohol", "quality"]].corr()
     print(corr)
 
     # 相関係数をヒートマップ化
     sns.heatmap(corr, annot=True)
+    # plt.show()
+    # plt.savefig("heatmap-figure.png")
+
+    return
+
+
+def plot_func(Y_train, Y_train_pred, Y_test, Y_test_pred):
+    plt.scatter(Y_train,  # グラフのx値(予測値)
+                Y_train_pred - Y_train,  # グラフのy値(予測値と学習値の差)
+                c='blue',  # プロットの色
+                marker='o',  # マーカーの種類
+                s=10,  # マーカーサイズ
+                alpha=0.7,  # 透明度
+                label='train data')  # ラベルの文字
+
+    # 予測値と残差をプロット（テストデータ）
+    plt.scatter(Y_test_pred,
+                Y_test_pred - Y_test,
+                c='red',
+                marker='o',
+                s=10,
+                alpha=0.7,
+                label='test data')
+
+    # グラフ書式設定
+    y_min = min(Y_train)
+    y_max = max(Y_train)
+
+    plt.xlabel('予測値')
+    plt.ylabel('残差')
+    plt.legend(loc='upper left')
+    plt.hlines(y=0, xmin=0, xmax=10, lw=2, color='black')
+    # plt.xlim([-20, 60])
+    # plt.ylim([y_min, y_max])
+    plt.tight_layout()
     plt.show()
+
+    return
 
 
 def main():
@@ -87,10 +154,16 @@ def main():
 
     X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=0)
 
-    # 実行
-    polynomial_regression(X_train, Y_train, X_test, Y_test, X, Y)
-    # lasso_regression(X_train, Y_train, X_test, Y_test)
+    # 相関係数を計算
+    # calculate_coer(X, Y)
 
+    # 実行
+    # linear_regression(X_train, Y_train, X_test, Y_test)
+    # polynomial_regression(X_train, Y_train, X_test, Y_test, X, Y)
+    lasso_regression(X_train, Y_train, X_test, Y_test)
+
+
+    return
 
 if __name__ == "__main__":
     main()
